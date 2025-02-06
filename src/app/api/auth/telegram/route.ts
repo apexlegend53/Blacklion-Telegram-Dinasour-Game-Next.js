7a411a4f0220ff9236231d82e5ee1ddc84a41773
Chandana.Abeyrathna@sparc.bot <Chandana.Abeyrathna@sparc.bot>
2025-02-06 04:47:48 -0800

    remove export routing problem

--- src/app/api/auth/telegram/route.ts
+++ src/app/api/auth/telegram/route.ts
@@ -18,7 +18,7 @@ const adminAccount = privateKeyToAccount({
   client,
 });
 
-export async function verifyTelegram(signature: string, message: string) {
+async function verifyTelegram(signature: string, message: string) {
   const metadata = JSON.parse(message);
 
   if (!metadata.expiration || metadata.expiration < Date.now()) {
